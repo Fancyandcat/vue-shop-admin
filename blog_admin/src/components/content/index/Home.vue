ea09971f4589ac0a599eca6efe81143944bd4d06
Fancy <fanchenyu@gracepsy.com>
2018-03-20 16:37:44 +0800

    测试leanCloud

--- blog_admin/src/components/content/index/Home.vue
+++ blog_admin/src/components/content/index/Home.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>我是首页，大兄弟</div>
+  <div>
+    <button @click="addData">click</button>
+  </div>
 </template>
+<script>
+export default {
+  data () {
+    return {}
+  },
+  created () {
+  },
+  methods: {
+    addData () {
+      var todo = window.AV.Object.createWithoutData('TestObject', '5ab0b77c75657116a39210f5')
+      todo.destroy().then(function (success) {
+        console.log('成功', success)
+      }, function (error) {
+        console.error(error)
+      })
+    }
+  }
+}
+</script>

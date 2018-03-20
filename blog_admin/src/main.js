e13ee0806a19a7fbbfa74754130f16f8b9813137
Fancy <fanchenyu@gracepsy.com>
2018-03-20 11:35:11 +0800

    将重构代码暂存

--- blog_admin/src/main.js
+++ blog_admin/src/main.js
@@ -6,9 +6,16 @@ import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
 import 'common/stylus/reset.styl'
+import AV from 'leancloud-storage'
+window.AV = AV
 Vue.use(ElementUI)
 Vue.config.productionTip = false
-
+var APP_ID = 'IPqWSvhf3TreD8bipidSCJoz-gzGzoHsz'
+var APP_KEY = 's8eGizSK6wpJFWaV5SlDWo4G'
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',

b57dea80c0143f6da5ee6d6904a844e114713b4e
fancy <1820827184@qq.com>
2018-04-21 10:16:56 +0800

    no info

--- blog_admin/src/main.js
+++ blog_admin/src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 新婚快乐
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
